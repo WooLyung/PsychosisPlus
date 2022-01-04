a742398c7e9ca1d4d66751120c2ca40c11f1a613
WooLyung <vlfkdydtk@naver.com>
2022-01-04 20:10:28 +0900

    add first hediff

---
+++ Source/Source/ThoughtWorker/ThoughtWorker_PTSD.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using System;
+using Verse;
+
+namespace PsychosisPlus
+{
+    public class ThoughtWorker_PTSD : RimWorld.ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            foreach (var hediff in p?.health?.hediffSet?.GetHediffs<Hediff_PTSD>())
+                return ThoughtState.ActiveDefault;
+            return ThoughtState.Inactive;
+            throw new NotImplementedException();
+        }
+    }
+}

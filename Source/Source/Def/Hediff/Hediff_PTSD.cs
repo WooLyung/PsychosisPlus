47a36454a3fe2bc0b211c1773b9c2b05f76bb94f
WooLyung <vlfkdydtk@naver.com>
2022-01-06 21:40:44 +0900

    add event observer

--- Source/Source/Def/Hediff/Hediff_PTSD.cs
+++ Source/Source/Def/Hediff/Hediff_PTSD.cs
@@ -4,10 +4,21 @@ using Verse;
 
 namespace PsychosisPlus
 {
-    public class Hediff_PTSD : Hediff_DangerousPsychosis
+    public class Hediff_PTSD : Hediff_DangerousPsychosis, PawnDamage_Catcher.Observer
     {
-        public override void Notify_PawnPostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
+        public Hediff_PTSD()
         {
+            PawnDamage_Catcher.observers.Add(this);
+        }
+
+        ~Hediff_PTSD()
+        {
+            PawnDamage_Catcher.observers.Remove(this);
+        }
+
+        public void Notify(ref Pawn pawn, DamageInfo dinfo, float totalDamageDealt)
+        {
+            Log.Message(pawn.Name.ToString());
         }
     }
 }
--- Source/Source/Patch/Event_Catcher.cs
+++ Source/Source/Patch/Event_Catcher.cs
@@ -10,9 +10,17 @@ namespace PsychosisPlus
     [HarmonyPatch(new Type[] { typeof(DamageInfo), typeof(float) })]
     public class PawnDamage_Catcher
     {
+        public interface Observer
+        {
+            void Notify(ref Pawn pawn, DamageInfo dinfo, float totalDamageDealt);
+        }
+
+        public static List<Observer> observers = new List<Observer>();
+
         static void Postfix(ref Pawn __instance, DamageInfo dinfo, float totalDamageDealt)
         {
-            Log.Message(__instance.Name.ToString());
+            foreach (var observer in observers)
+                observer.Notify(ref __instance, dinfo, totalDamageDealt);
         }
     }
 
@@ -21,9 +29,17 @@ namespace PsychosisPlus
     [HarmonyPatch(new Type[] { typeof(IntVec3), typeof(Map), typeof(float), typeof(DamageDef), typeof(Thing), typeof(int), typeof(float), typeof(SoundDef), typeof(ThingDef), typeof(ThingDef), typeof(Thing), typeof(ThingDef), typeof(float), typeof(int), typeof(bool), typeof(ThingDef), typeof(float), typeof(int), typeof(float), typeof(bool), typeof(float?), typeof(List<Thing>) })]
     public class Explosion_Catcher
     {
+        public interface Observer
+        {
+            void Notify(IntVec3 center, Map map, float radius, DamageDef damType, Thing instigator, int damAmount = -1, float armorPenetration = -1f, SoundDef explosionSound = null, ThingDef weapon = null, ThingDef projectile = null, Thing intendedTarget = null, ThingDef postExplosionSpawnThingDef = null, float postExplosionSpawnChance = 0f, int postExplosionSpawnThingCount = 1, bool applyDamageToExplosionCellsNeighbors = false, ThingDef preExplosionSpawnThingDef = null, float preExplosionSpawnChance = 0f, int preExplosionSpawnThingCount = 1, float chanceToStartFire = 0f, bool damageFalloff = false, float? direction = null, List<Thing> ignoredThings = null);
+        }
+
+        public static List<Observer> observers = new List<Observer>();
+
         static void Postfix(IntVec3 center, Map map, float radius, DamageDef damType, Thing instigator, int damAmount = -1, float armorPenetration = -1f, SoundDef explosionSound = null, ThingDef weapon = null, ThingDef projectile = null, Thing intendedTarget = null, ThingDef postExplosionSpawnThingDef = null, float postExplosionSpawnChance = 0f, int postExplosionSpawnThingCount = 1, bool applyDamageToExplosionCellsNeighbors = false, ThingDef preExplosionSpawnThingDef = null, float preExplosionSpawnChance = 0f, int preExplosionSpawnThingCount = 1, float chanceToStartFire = 0f, bool damageFalloff = false, float? direction = null, List<Thing> ignoredThings = null)
         {
-            Log.Message("explosion");
+            foreach (var observer in observers)
+                observer.Notify(center, map, radius, damType, instigator, damAmount, armorPenetration, explosionSound, weapon, projectile, intendedTarget, postExplosionSpawnThingDef, postExplosionSpawnChance, postExplosionSpawnThingCount, applyDamageToExplosionCellsNeighbors, preExplosionSpawnThingDef, preExplosionSpawnChance, preExplosionSpawnThingCount, chanceToStartFire, damageFalloff, direction, ignoredThings);
         }
     }
 }
